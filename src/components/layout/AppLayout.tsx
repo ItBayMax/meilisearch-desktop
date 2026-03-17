7a3e88b9c5ddc6d4607ac119f14cd25531b17c84
ItBayMax <szm@kaiyufound.com>
2026-03-17 15:17:30 +0800

    style(app): optimize dragging area compatibility on macOS

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,12 +4,8 @@ import { Sidebar } from "./Sidebar";
 
 export function AppLayout() {
   return (
-    <div className="h-screen flex flex-col overflow-hidden">
-      {/* Drag region for Tauri */}
-      <div
-        data-tauri-drag-region
-        className="h-8 w-full flex-shrink-0 bg-background"
-      />
+    <div className="h-screen flex flex-col overflow-hidden pt-8">
+      {/* Note: macOS drag layer is in index.html for WKWebView compatibility */}
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <div className="flex-1 flex flex-col overflow-hidden">
